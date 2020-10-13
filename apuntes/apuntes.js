088447985f0447fe456d7b7409d70a04754bbae7
unknown <alejandrojzeballos@gmail.com>
2020-10-13 03:20:54 -0400

    Explicacion de CLI y verificando instalaciones

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -4,7 +4,31 @@
         nodejs
         visual
         docker
-    
+    CLI: es un metodo que permite dar instrucciones a algun programa con una linea de texto simple.(bash,powersheel, terminal).
 
+        S.O. ls,mkdir,cd
+            ej mkdir myfolder
+        NODE.js:
+            node (palabra reservada)
+            node -v(palabra reservada,argumento)
+            ej node app.js
+        NPM.
+            npm (palabra reservada)
+            npm -v(palabra reservada,argumento)
+            ej npm i express
+        GIT
+            git (palabra reservada)
+            git pull(palabra reservada,argumento)
+            ej git status
+        Docker
+            docker (palabra reservada)
+            docker pull(palabra reservada,argumento)
+    estos programas ya viene con el CLI integrado.
+    Para verifiar las instalaciones ejecutamos los siguientes comandos:
+                        node -v
+                        npm -v
+                        git --version
+                        docker -v
+    
 
 */
