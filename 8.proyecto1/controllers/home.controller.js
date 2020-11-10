0ccd1b6e33b596cb5498b009d512a945b6669ec3
unknown <alejandrojzeballos@gmail.com>
2020-11-10 02:28:40 -0400

    renderizando las vista de home y about

--- 8.proyecto1/controllers/home.controller.js
+++ 8.proyecto1/controllers/home.controller.js
@@ -1,6 +1,17 @@
+const path = require('path');
+function render(file,res){
+    return res.sendFile(path.join(__dirname + `/../views/${file}.html`))
+    //renderizamos la vista
+}
 class HomeController{
     async index(req,res){
-        return res.send({message : "Hello world!"});
+       // return res.send({message : "Hello world!"});
+        //queremos que se renderize la view home
+        return render("home",res);
+    }
+
+    async about(req,res){
+         return render("about",res);
     }
 }
 module.exports = new HomeController();
--- 8.proyecto1/routes/home.routes.js
+++ 8.proyecto1/routes/home.routes.js
@@ -3,4 +3,5 @@ const router = require("express").Router();
 const {HomeController} = require("../controllers");
 const homeController = require("../controllers/home.controller");
 router.get("/", homeController.index);  //en esta ruta se ejecutara index
+router.get("/about",homeController.about);
 module.exports = router;
