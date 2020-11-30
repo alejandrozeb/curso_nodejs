94e867b2bc4fa03a9f1a4dbe5e607d17e3e5debf
unknown <alejandrojzeballos@gmail.com>
2020-11-29 23:51:29 -0400

    confirmando la conexion

--- 11-apitech/backend/index.js
+++ 11-apitech/backend/index.js
@@ -5,7 +5,13 @@ const {PORT,MONGO_URI} = require("./config");
 
 const mongoose= require('mongoose');
 //mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology:true});
-mongoose.conect('mongodb://localhost', { useUnifiedTopology: true });
+mongoose.conect('mongodb://localhost', { useUnifiedTopology: true })
+.then(()=>{
+    server.listen(PORT, () =>{
+        console.log(`CodingApp backend runnig on port ${PORT}`);
+    });
+})
+.catch(console.log)
 
 server.listen(PORT, () =>{
     console.log(`codingApp backend runnig on port: ${PORT}`);
