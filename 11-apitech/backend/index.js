fc9d3708485c38f5875190086a0bb22765d509d8
unknown <alejandrojzeballos@gmail.com>
2020-12-22 00:46:45 -0400

    index principal configurado con mongo atlas

--- 11-apitech/backend/index.js
+++ 11-apitech/backend/index.js
@@ -4,8 +4,12 @@ const server = require('./server');
 const {PORT,MONGO_URI} = require("./config");
 
 const mongoose= require('mongoose');
-//mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology:true});
-mongoose.connect('mongodb://localhost', { useUnifiedTopology: true })
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser: true, 
+    useUnifiedTopology:true,
+    useCreateIndex: true
+})
+/* mongoose.connect('mongodb://localhost', { useUnifiedTopology: true }) */
 .then(()=>{
     server.listen(PORT, () =>{
         console.log(`CodingApp backend runnig on port ${PORT}`);
@@ -13,6 +17,6 @@ mongoose.connect('mongodb://localhost', { useUnifiedTopology: true })
 })
 .catch(console.log)
 
-server.listen(PORT, () =>{
+/* server.listen(PORT, () =>{
     console.log(`codingApp backend runnig on port: ${PORT}`);
-});
+}); */
--- 11-apitech/backend/server/index.js
+++ 11-apitech/backend/server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const server = express();
-
+//midlewares
+server.use(express.json());
+server.use(express.static(__dirname+"../public"));
 module.exports = server;
