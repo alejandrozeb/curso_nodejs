dfa295c668a6bdf9b8e957e134c7d1bd8bcdf756
unknown <alejandrojzeballos@gmail.com>
2020-12-22 00:58:38 -0400

    bug con el server al importar la carpeta publica

--- 11-apitech/backend/server/index.js
+++ 11-apitech/backend/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
+const cors = require("cors");
 //midlewares
 server.use(express.json());
-server.use(express.static(__dirname+"../public"));
+server.use(express.static(__dirname+"/../public"));
+server.use(cors());
 module.exports = server;
