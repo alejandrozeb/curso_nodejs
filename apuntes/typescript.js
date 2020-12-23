80e7844e0a512f7cfaa4862e9b8334888f91f843
unknown <alejandrojzeballos@gmail.com>
2020-12-22 22:54:58 -0400

    iniciando un proyecto con typescript

---
+++ apuntes/typescript.js
@@ -0,0 +1,12 @@
+/* 
+----------TYPESCRIPT---------
+iniciamos una consola en la carpeta de la seccion de ts-
+tsc --version muestra la version de typescript en nuestra maquina
+
+tsc --init  crea un json con configuracion de ts
+    target es la version de emacscript
+    module es donde maneja los moduos node
+    strict  ts sera estricto
+    esModuleInterop interaccion con nodejs
+
+*/
