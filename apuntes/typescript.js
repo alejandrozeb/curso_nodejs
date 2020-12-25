539c9c60b97575833ec3f2ac7e1846ad79896472
unknown <alejandrojzeballos@gmail.com>
2020-12-25 01:26:46 -0400

    rutas definidas con el enrutador de angular

--- apuntes/typescript.js
+++ apuntes/typescript.js
@@ -91,6 +91,27 @@ noes buena practica usar otra libreria como jquery.
 [routerLink]="['home']"
 es una etiqueta de angular que enruta el a, 
 
+enrutamos nuestros componentes 
+    {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'technologies', component: TechnologiesComponent
+  },
+  {
+    path: 'technology/:id', component: TechnologyComponent  
+  },
+  {
+    path: 'search/:query', component: SearchComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: "home"
+  }
 
 
 */
