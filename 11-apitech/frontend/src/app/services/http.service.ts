0179a60a707ad245d73b5c952ca80b3786adebb7
unknown <alejandrojzeballos@gmail.com>
2020-12-26 02:32:23 -0400

    configuranado el environment con las variables de entorno y configurando las rutas a la api que usaremos

--- 11-apitech/frontend/src/app/services/http.service.ts
+++ 11-apitech/frontend/src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {Technology} from "../models/technology.model";
+import {environment} from "src/environments/environment"
 @Injectable({
   providedIn: 'root'
 })
@@ -9,5 +10,18 @@ export class HttpService {
   es lo mismo que iniciar en el constructor
   private _http: HttpClient;
 */
+  private baseUrl: string= environment.BASE_API_URL;
   constructor(private readonly _http: HttpClient) { }
+
+  public getTechnologies(){
+    return this._http.get<Technology[]>(this.baseUrl + "/technologies")
+  }
+  public getTechnology(id:string){
+    return this._http.get<Technology>(this.baseUrl+"/technology/"+id);
+  }
+
+  public searchTechnology (query:string){
+    return this._http.get<Technology>(this.baseUrl+"/technology/search"+query);  
+  }
+  //debemos colocar el generico para detectar que va a devolver
 }
