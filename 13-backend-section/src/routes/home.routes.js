3bbf238914329bf8b29e73027f7e71ca71be945a
unknown <alejandrojzeballos@gmail.com>
2021-01-01 02:00:45 -0400

    añadiendo la ruta con la dependencia

---
+++ 13-backend-section/src/routes/home.routes.js
@@ -0,0 +1,10 @@
+const {Router} = require("express");
+
+module.exports = function({HomeController}){
+    const router = Router();
+
+    router.get("/", HomeController.index);
+    //el scope es del express, pero con la injeccoin de depedendencia podemos usar nuestro servicio
+
+    return router;
+};
