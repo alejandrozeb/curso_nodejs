9720cdca70e7f4c7d0a5980023d0342bb9a0d43b
unknown <alejandrojzeballos@gmail.com>
2021-01-01 02:42:37 -0400

    index de router configurado

---
+++ 13-backend-section/src/routes/index.js
@@ -0,0 +1,27 @@
+//router principal, encargado del middleware principal
+
+const express = require('express');
+const cors = require('cors');
+const  helmet= require('helmet');
+const  compression= require('compression');
+require('express-async-errors');
+
+module.exports = function({HomeRoutes}){
+    const router = express.Router();
+    const apiRoutes = express.Router();
+
+    apiRoutes
+    .use(express.json)
+    .use(cors())
+    .use(helmet())
+    .use(compression());
+    //estos son los iddlewares que se ejecutan al principio default
+
+    apiRoutes.use("/home", HomeRoutes);
+
+    router.use("/v1/api", apiRoutes);
+    //todas las rutas van a tener v1/api
+    //escalabilidad configurada
+
+    return router;
+}
