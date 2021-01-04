8b7571f470e05e01179dc18949faf550a06b2bd6
unknown <alejandrojzeballos@gmail.com>
2021-01-04 02:11:43 -0400

    Primer hook configurado

--- 13-backend-section/src/models/úser.model.js
+++ 13-backend-section/src/models/úser.model.js
@@ -22,6 +22,33 @@ const UserSchema = new Schema({
     }
 });
 
+UserSchema.methods.toJSON = function(){
+    let user= this.toObject();
+    delete user.password;
+    return user;
+};
+
+//hooks
+
+UserSchema.methods.comparePasswords = function(password){
+    return compareSync(password, this.password);
+}
+
+
+//este es un hook para que cada vez se guarde un documento con mongo
+UserSchema.pre('save',async function(next){
+    const user = this; //corresponde al elemento que se esta procesando
+
+    //validando la contraseña
+    if(!user.isModified("password")){
+        return next();
+    }
+    const salt = genSaltSync(10);
+    const hashedPassword = hashSync(user.password, salt);
+    user.password = hashedPassword;
+    next();
+});
+//antes que se guarde ejecutadra una funcion asyncronica
 //metodos
 
 
