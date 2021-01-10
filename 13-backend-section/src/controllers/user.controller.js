2dabeb173a4e7a111ca3e594307a41b15b93ac0f
unknown <alejandrojzeballos@gmail.com>
2021-01-10 01:01:39 -0400

    controller de user añadido

---
+++ 13-backend-section/src/controllers/user.controller.js
@@ -0,0 +1,36 @@
+let _userService = null;
+
+class UserController {
+    constructor({UserService}){
+        _userService = UserService;
+    }
+
+    async get(req,res){
+        const { userId } = req.params; //sadasd/asdasd/id
+        const user = await _userService.get(userId);
+
+        return res.send(user);
+    }
+
+    async getAll(req, res){
+        const users = await _userService.getAll();
+        return res.send(user);
+    }
+
+    async update(req,res){
+        const {body} = req; 
+        const {userId} = req.params;
+        const update = await _userService.update(userId,body);
+
+        return res.send(updatedUser);
+    }
+
+    async delete (req,res){
+        const { userId } = req.params;
+        const deletedUser = await _userService.delete(userId);
+        return res.send(deletedUser);
+    }
+
+}
+
+module.exports = UserController;
