2423f57960585c586b9a4c24120a5414e85d6a47
unknown <alejandrojzeballos@gmail.com>
2021-01-13 02:28:32 -0400

    configurando el index.js de las rutas

--- 13-backend-section/src/routes/index.js
+++ 13-backend-section/src/routes/index.js
@@ -8,7 +8,10 @@ require("express-async-errors");
 const {ErrorMiddleware,NotFoundMiddleware} = require("../middlewares");
 
 module.exports = function({
-    HomeRoutes
+    HomeRoutes,
+    UserRoutes,
+    IdeaRoutes,
+    CommentRoutes
 }) {
     const router = express.Router();
     const apiRoutes = express.Router();
@@ -21,6 +24,9 @@ module.exports = function({
     //estos son los iddlewares que se ejecutan al principio default
 
     apiRoutes.use("/home", HomeRoutes);
+    apiRoutes.use("/user", UserRoutes);
+    apiRoutes.use("/idea", IdeaRoutes);
+    apiRoutes.use("/comment", CommentRoutes);
 
     router.use("/v1/api", apiRoutes);
     //todas las rutas van a tener v1/api
