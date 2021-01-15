a99694ceb86f2b7c3f339e354c4ae5cd44f41b86
unknown <alejandrojzeballos@gmail.com>
2021-01-15 01:39:46 -0400

    introduccion a docker

--- apuntes/docker.js
+++ apuntes/docker.js
@@ -1,6 +1,32 @@
 /* 
 Docker
 
+Introduccion
+LA idea de docker es crear contenedores ligeros y portables para las aplicaciones de software que puedan ejecutarse en cualquier maquina con docker instalado, independientemente del sistema opertivo, facilitando asi tambien los despliegues.
+
+
+Contenedor
+Viene de linux, un contenedor puede llevarse de un lado al otro de manera independiente.
+app web
+servidor
+dependencias nodejs nom mongo nginx
+aplicacion
+
+docker nos permite agregar estas dependencias y podemos llevar a cualquier maquina sin preocuparse de los elementos externos como compatibilidad, versionamiento de dependencias.
+
+beneficios 
+es una herramienta para beneficiar a los dev testers y devops
+
+Hace que el desarrollador pueda centrarse en el codigo
+
+En mi mauina funciona ya no aplica para esto
+
+se puede hacer testing, y iiniciar en diferentes maquinas
+ahorra costo.
+
+es opensource.
+
+
 
 
 
