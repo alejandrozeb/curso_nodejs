5a47de5eb75c595f95501c9e6c79641f64583b33
unknown <alejandrojzeballos@gmail.com>
2021-01-16 22:25:34 -0400

    añadiendo el helper para generar tokens por usuario jwthelper

--- 13-backend-section/src/helpers/jwt.helper.js
+++ 13-backend-section/src/helpers/jwt.helper.js
@@ -1,3 +1,9 @@
 //es una funcion que se exportara usaremos json web token
 
-const {} = require("jsonwebtoken"); //sirve para firma cada token
+const {sign} = require("jsonwebtoken"); //sirve para firma cada token
+
+const {JWT_SECRET} = require("../config");
+
+modules.exports.generateToken = function(user){
+    return sign({user}, JWT_SECRET, {expiresIn:"4h"});//retorna un token con el nombre del usuario
+}
