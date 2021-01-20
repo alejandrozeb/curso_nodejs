df8f56e62a2a979af427effe2ca9860d479ab872
unknown <alejandrojzeballos@gmail.com>
2021-01-20 01:56:33 -0400

    añadiendo parseint middleware para cambiar las url que llegan con ids

---
+++ 13-backend-section/src/middlewares/parse-int.middleware.js
@@ -0,0 +1,17 @@
+module.exports = function(req,res,next){
+    const queryStrings = req.query;
+    //mysql.com?id:5
+    //debemos parsear el numero de la url
+
+    for(const key in queryStrings){
+        const length = queryStrings[key].length;
+        const isValid = length > 20 ? false : !isNaN(parseInt(queryStrings[key]));
+
+        if(isValid){
+            queryStrings[key] = parseInt(queryStrings[key]);
+        }
+    }
+
+    req.query = queryStrings;
+    next();
+}
