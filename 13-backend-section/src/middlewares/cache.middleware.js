8095d90fd0dd74d337bf52d1e051e62796b289fe
unknown <alejandrojzeballos@gmail.com>
2021-01-21 01:47:05 -0400

    añadiendo el cachemiddleware

--- 13-backend-section/src/middlewares/cache.middleware.js
+++ 13-backend-section/src/middlewares/cache.middleware.js
@@ -1,1 +1,21 @@
 const mcache = require("memory-cache");
+const {CACHE_KEY} = require("../config");
+
+module.exports = function(req,res,next){
+    return (req,res, next) =>{
+        const key = CACHE_KEY + req.originUrl || req.url;
+        const cachedbody = mcache.get(cachedbody);
+
+        if(cachedbody){
+            return res.send(JSON.parse(cachedbody));
+        }else{
+            res.sendResponse = res.send;
+            res.send = body => {
+                mcache.put(key,body, duration * 1000);
+                res.sendResponse(body);
+                //cachea body
+            };
+            next();
+        }
+    }
+}
